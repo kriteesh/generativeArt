45ac48d1321e057b6011709374feedf22ce03f00
kriteesh <kriteesh.parashar@gmail.com>
2018-07-08 21:39:25 +0530

    chaos file

---
+++ scripts/ballBouncing.js
@@ -0,0 +1,39 @@
+let canvas  = document.querySelector("canvas");
+let w = canvas.width = document.getElementsByClassName("main")[0].clientWidth;
+let h = canvas.height = document.getElementsByClassName("main")[0].clientHeight;
+let ctx = canvas.getContext("2d");
+			
+let x_o = w/2;
+let y_o = h/2;
+
+
+let setting = (x,y) =>{
+		ctx.fillStyle = "black";
+		ctx.fillRect(0,0,w,h);
+		ctx.fillStyle = "white";
+		ctx.beginPath();
+		ctx.arc(x,y,10,0,2*Math.PI);
+		ctx.fill();
+		}
+
+let x = x_o;
+let y = y_o;
+let dx = 1.1;
+let dy = 2.2;
+			
+let animate =() =>{
+	ctx.clearRect(0,0,w,h);
+	requestAnimationFrame(animate);			
+
+	if((x>=w-10)||(x-10<=0)) dx = -dx;
+	if((y>=h-10)||(y-10<=0)) dy = -dy;
+				
+	x = x + dx;
+	y = y + dy;
+					
+	setting(x,y);
+					
+
+}
+			
+animate();
--- scripts/generative.js
+++ scripts/generative.js
@@ -1,23 +1,26 @@
-	const par_curve = (y) => (x) => (t_in) => (t_fin) => (dt) => (color) =>
+const par_curve = (y) => (x) => (t_in) => (t_fin) => (dt) => (color) =>
 		{
 		
 		ctx.shadowColor = color;
 		ctx.strokeStyle = color;
 		ctx.fillStyle = color;
 		
-		
 		if(t_in >= t_fin)
 		{
-		ctx.arc(x_o + x(t_in), y_o - y(t_in),2,0,2*Math.PI); //terminal points//
-		ctx.fill();
+			ctx.arc(x_o + x(t_in), y_o - y(t_in),2,0,2*Math.PI); //terminal points//
+			ctx.fill();
 		}
 		else
 		{
-		ctx.beginPath();
-		ctx.moveTo(x_o + x(t_in), y_o - y(t_in));
-		ctx.lineTo(x_o + x(t_in + dt), y_o - y(t_in + dt));
-		ctx.stroke();
-		
-		return par_curve(y)(x)(t_in + dt)(t_fin)(dt)(color);
+			ctx.beginPath();
+			ctx.moveTo(x_o + x(t_in), y_o - y(t_in));
+			ctx.lineTo(x_o + x(t_in + dt), y_o - y(t_in + dt));
+			ctx.stroke();
+
+			return par_curve(y)(x)(t_in + dt)(t_fin)(dt)(color);
 		}
-		}
+	}
+
+const transform_point = x_y => theta => [x_y[0]*Math.cos(theta) - x_y[1]*Math.sin(theta), x_y[1]*Math.cos(theta)  + x_y[0]*Math.sin(theta)]
+
+
